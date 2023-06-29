725cf736fddf7f2d602722952aa93c2e1c18a9ff
brunachavesb <brunachavesb@gmail.com>
2023-06-29 01:01:13 -0300

    feat: :sparkles: adicionado devise do usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
-  validates :name, length: { maximum: 255 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nome, :email, presence: true
+  validates :nome, length: { maximum: 255 }
   validates :email, length: { maximum: 255 }
 end
