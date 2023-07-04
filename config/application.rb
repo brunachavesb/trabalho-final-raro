c337e438890f9b042e98d1210af24fe7b2340cf7
brunachavesb <brunachavesb@gmail.com>
2023-07-04 01:42:14 -0300

    feat: :sparkles: environments configurado com dotenv

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module TrabalhoFinalBrunaBarreto
     I18n.available_locales = [:en, :pt]
     config.i18n.default_locale = :pt
 
+    Dotenv.load
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
