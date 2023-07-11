dd190e22b56dea78b234d987cd6c64e68c264b5f
brunachavesb <brunachavesb@gmail.com>
2023-07-10 22:15:25 -0300

    feat: :sparkles: adicionado controller e view para criação de novas tarefas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,19 @@ class TasksController < ApplicationController
     @tasks = Task.order(created_at: :desc).paginate(page: params[:page], per_page: 20)
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Tarefa criada com sucesso."
+    else
+      render :new
+    end
+  end
+
   private
 
   def task_params
