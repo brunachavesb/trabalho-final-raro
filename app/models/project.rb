b8daa78838580cb10ad0df69e0f85c98100a7f24
brunachavesb <brunachavesb@gmail.com>
2023-07-12 22:15:05 -0300

    feat: :sparkles: models alterados

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ApplicationRecord
   belongs_to :user
 
+  has_many :tasks
+
   validates :project_id, presence: true
   validates :url, presence: true
   validates :name, presence: true, length: { maximum: 255 }
